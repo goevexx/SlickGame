922c456d03e5c0ee81533958b5855cbe9d8734d2
Aaron Visang <a.visang@t-online.de>
2017-12-13 12:43:28 +0100

    added Shot.java

---
+++ src/de/mankodach/TypingGame/Shot.java
@@ -0,0 +1,15 @@
+package de.mankodach.TypingGame;
+
+import org.newdawn.slick.Color;
+
+public class Shot {
+	private Color color;
+	
+	public Color getColor() {
+		return color;
+	}
+
+	public Shot(Color color){
+		this.color = color;
+	}
+}
