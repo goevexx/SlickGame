a8cfaeadec45e3d8cc76eead61b02b80d9bf0c9a
Goevex <nicolas.morawietz@googlemail.com>
2017-12-13 20:41:09 +0100

    Erster Push für Enemy und Game

---
+++ src/de/mankodach/TypingGame/Enemy.java
@@ -0,0 +1,36 @@
+package de.mankodach.TypingGame;
+
+import org.newdawn.slick.Color;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.geom.Rectangle;
+import org.newdawn.slick.geom.Shape;
+
+public class Enemy {
+	private Color color;
+	private String word;
+	
+	public Rectangle rec;
+	private int width;
+	private int height;
+
+	public Enemy(Color color, String word) {
+		super();
+		this.color = color;
+		this.word = word;
+		this.width = 100;
+		this.height = 100;
+		this.rec = new Rectangle(0,0,this.width, this.height);
+	}
+	
+	public void draw(Graphics g){
+		g.fill(this.rec);
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+}
