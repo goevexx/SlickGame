3d8ea3c328d5e8efd9fd014468d0099b36e507d6
Nicolas Morawietz <nicolas.morawietz@googlemail.com>
2017-12-18 21:58:34 +0100

    Several changes the statebasegame to work
    Don't forget CTRL+SHIFT+F

--- src/de/mankodach/TypingGame/Button.java
+++ src/de/mankodach/TypingGame/Button.java
@@ -10,9 +10,9 @@ public class Button {
 	private float y;
 	private float width;
 	private float height;
-	
+
 	private Rectangle rec;
-	
+
 	public Button(String text, float x, float y, float width, float height) {
 		super();
 		this.text = text;
@@ -20,18 +20,20 @@ public class Button {
 		this.y = y;
 		this.width = width;
 		this.height = height;
+		this.rec = new Rectangle(x, y, width, height);
 	}
 
 	public void draw(Graphics g) {
 		Color gColor = g.getColor();
 		g.setColor(Color.white);
-		g.fill(new Rectangle(this.x,this.y,this.width,this.height));
+		g.fill(this.rec);
 		g.setColor(Color.black);
-		g.drawString(this.text, this.x+width/2-g.getFont().getWidth(this.text)/2, this.y+height/2-g.getFont().getHeight(this.text)/2);
+		g.drawString(this.text, this.x + width / 2 - g.getFont().getWidth(this.text) / 2,
+				this.y + height / 2 - g.getFont().getHeight(this.text) / 2);
 		g.setColor(gColor);
 	}
-	
+
 	public boolean contains(float x, float y) {
-		return rec.contains(x,y);
+		return rec.contains(x, y);
 	}
 }
--- src/de/mankodach/TypingGame/TypingGame.java
+++ src/de/mankodach/TypingGame/TypingGame.java
@@ -1,41 +1,45 @@
 package de.mankodach.TypingGame;
 
+import java.util.ArrayList;
+
 import org.newdawn.slick.*;
 import org.newdawn.slick.state.StateBasedGame;
 
-public class TypingGame extends StateBasedGame{
+public class TypingGame extends StateBasedGame {
 	public static final String gamename = "Crazy typing Game!";
-	public enum States { mainMenuState, gameState, settingsState }
-	
+
+	public enum States {
+		mainMenuState, gameState, settingsState
+	}
+
 	public MainMenu mainMenu;
-//	public Game game;
-//	public Settings settings;
+	public Game game;
+	public Settings settings;
 
 	public TypingGame() {
 		super(gamename);
 		mainMenu = new MainMenu(States.mainMenuState.ordinal());
-//		game = new Game();
-//		settings = new Settings();
-		
-		this.addState(mainMenu);
-//		this.addState(game);
-//		this.addState(Settings);
-		System.out.println(this.getStateCount());
+		settings = new Settings(States.settingsState.ordinal());
+		game = new Game(Color.red, new ArrayList<String>(), States.gameState.ordinal());
 	}
 
 	public static void main(String[] args) throws SlickException {
 		AppGameContainer container = new AppGameContainer(new TypingGame());
-		try {
-		container.setDisplayMode(1280, 720, false);
-		container.start();
-		}catch(SlickException e) {
+		try 
+		{
+			container.setDisplayMode(1280, 720, false);
+			container.setAlwaysRender(true);
+			
+			container.start();
+		} catch (SlickException e) {
 			e.printStackTrace();
 		}
 	}
 
 	@Override
 	public void initStatesList(GameContainer container) throws SlickException {
-		this.getState(States.mainMenuState.ordinal()).init(container, this);
-		this.enterState(States.mainMenuState.ordinal());
+		this.addState(mainMenu);
+		this.addState(game);
+		this.addState(settings);
 	}
 }
