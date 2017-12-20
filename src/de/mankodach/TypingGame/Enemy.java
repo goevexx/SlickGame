3b4fca0bb1376e1fc2919aeee7806f767af51b6d
Nicolas Morawietz <nicolas.morawietz@googlemail.com>
2017-12-20 14:53:53 +0100

    Added draw functions

--- src/de/mankodach/TypingGame/Enemy.java
+++ src/de/mankodach/TypingGame/Enemy.java
@@ -3,34 +3,74 @@ package de.mankodach.TypingGame;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.geom.Rectangle;
-import org.newdawn.slick.geom.Shape;
 
 public class Enemy {
 	private Color color;
-	private String word;
-	
-	public Rectangle rec;
+	private Word word;
 	private int width;
 	private int height;
+	private int x;
+	private int y;
+	public Rectangle rec;
 
-	public Enemy(Color color, String word) {
+	public Enemy(int x, int y, Color color, Word word) {
 		super();
 		this.color = color;
 		this.word = word;
-		this.width = 100;
-		this.height = 100;
-		this.rec = new Rectangle(0,0,this.width, this.height);
+		this.x = x;
+		this.y = y;
 	}
-	
-	public void draw(Graphics g){
+
+	public void draw(Graphics g, int x, int y) {
+		Color gColor = g.getColor();
+		g.setColor(Color.white);
 		g.fill(this.rec);
+		g.setColor(Color.black);
+		if (this.rec == null) {
+			this.width = g.getFont().getWidth(this.word.getName()) + 10;
+			this.height = g.getFont().getHeight(this.word.getName()) + 10;
+			this.rec = new Rectangle(x, y, this.width, this.height);
+		} else {
+			rec.setX(x);
+			rec.setY(y);
+		}
+		this.word.draw(g,
+				this.x + width / 2 - g.getFont().getWidth(this.word.getName())
+						/ 2,
+				this.y + height / 2
+						- g.getFont().getHeight(this.word.getName()) / 2);
+		g.setColor(gColor);
 	}
 
-	public int getWidth() {
-		return width;
+	public Color getColor() {
+		return color;
 	}
 
-	public void setWidth(int width) {
-		this.width = width;
+	public void setColor(Color color) {
+		this.color = color;
+	}
+
+	public Word getWord() {
+		return word;
+	}
+
+	public void setWord(Word word) {
+		this.word = word;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
 	}
 }
--- src/de/mankodach/TypingGame/Player.java
+++ src/de/mankodach/TypingGame/Player.java
@@ -1,12 +1,53 @@
 package de.mankodach.TypingGame;
 
 import org.newdawn.slick.Color;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.geom.Circle;
 
 public class Player {
-
 	private Color color;
 	private int score;
 	private int lifepoints;
+	private int x;
+	private int y;
+	public Circle circ;
+
+	public Player(int x, int y, Color color) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.color = color;
+		this.score = 0;
+		this.lifepoints = 3;
+		this.circ = new Circle(x, y, 50);
+	}
+
+	public void draw(Graphics g) {
+		Color gColor = g.getColor();
+		g.setColor(color);
+		g.fill(this.circ);
+		g.setColor(gColor);
+	}
+	
+	public void kill(){
+		
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
 
 	public Color getColor() {
 		return color;
@@ -31,7 +72,7 @@ public class Player {
 	public void subScore(int score) {
 		this.score -= score;
 	}
-	
+
 	public int getLifepoints() {
 		return lifepoints;
 	}
@@ -39,7 +80,7 @@ public class Player {
 	public void setLifepoints(int lifepoints) {
 		this.lifepoints = lifepoints;
 	}
-	
+
 	public void addLifepoints(int lifepoints) {
 		this.lifepoints += lifepoints;
 	}
