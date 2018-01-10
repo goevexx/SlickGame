351a000a131ec71763a9419124564942bc6cc92a
Nicolas Morawietz <nicolas.morawietz@googlemail.com>
2018-01-10 23:30:43 +0100

    Auto stash before merge of "master" and "origin/master"

--- src/de/mankodach/TypingGame/Shot.java
+++ src/de/mankodach/TypingGame/Shot.java
@@ -5,20 +5,22 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.Sound;
 
-public class Shoot {
+public class Shot {
 	private float fromX;
 	private float fromY;
 	private float toX;
 	private float toY;
+	private Color color;
 	private Sound shootSound;
 	private boolean playShootSound;
 
-	public Shoot(float fromX, float fromY, float toX, float toY) {
+	public Shot(float fromX, float fromY, float toX, float toY,Color color) {
 		super();
 		this.fromX = fromX;
 		this.fromY = fromY;
 		this.toX = toX;
 		this.toY = toY;
+		this.color = color;
 		this.playShootSound = true;
 		try {
 			shootSound = new Sound("res/shoot.wav");
@@ -29,7 +31,7 @@ public class Shoot {
 
 	public void draw(Graphics g) {
 		Color gColor = g.getColor();
-		g.setColor(Color.white);
+		g.setColor(color);
 		g.drawLine(fromX, fromY, toX, toY);
 		g.setColor(gColor);
 	}
